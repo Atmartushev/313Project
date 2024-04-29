c4c9b543fb331ea2a6773f44abc6320b3d4c6194
Atmartushev <atmartushev@gmail.com>
2024-04-29 13:51:58 -0500

    fix: changed cart logic

--- elitestore/cart/models.py
+++ elitestore/cart/models.py
@@ -4,7 +4,14 @@ from core.models import ProductVariation
 
 class Cart(models.Model):
     user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    product = models.ManyToManyField(ProductVariation)
 
     def total_price(self):
-        return sum(product.price for product in self.products.all())
+        return sum(item.product_variation.price * item.quantity for item in self.cartitem_set.all())
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product_variation = models.ForeignKey(ProductVariation, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.product_variation} (Quantity: {self.quantity})"
