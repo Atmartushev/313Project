5feb9186bc2b24f95501eb1d7a2f7cd2856a686e
Atmartushev <atmartushev@gmail.com>
2024-04-30 19:01:44 -0500

    feat: added str methods to models

--- elitestore/core/models.py
+++ elitestore/core/models.py
@@ -3,25 +3,40 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
 
 class Color(models.Model):
     name = models.CharField(max_length=50)
     hex_value = models.CharField(max_length=7, blank=True, null=True)  # For web color representation
+    
+    def __str__(self):
+        return self.name
 
 class Size(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)  # Additional details, if needed
 
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
 class ProductVariation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     stock = models.IntegerField()
 
     class Meta:
